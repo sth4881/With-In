89834bcf0fbd5bbbc7ae3394e216819d91c9ae3c
Jinho Song <sth4881@naver.com>
2021-05-21 00:20:43 +0900

    [Add] OCR.java 추가(Object Storage로부터 이미지를 불러오는 방식이 아닌 현재 디렉토리로부터 이미지를 받아오도록 수정 필요

---
+++ src/app/src/main/java/com/example/graduationproject/OCR.java
@@ -0,0 +1,81 @@
+package com.ocr;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.UUID;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class OCR {
+    public static void main(String[] args) {
+        String apiURL = "https://0e5de5a9aebe4da1bcd5ef84a78605f0.apigw.ntruss.com/custom/v1/6604/ebe336381e3a156e85375e32f99ee0a86a480f2747219998eff226d9234ee616/infer";
+        String secretKey = "YnBEZ2dMTVljTkxrQ0FmS0Z2bll2SXdoenF4Z01KTXM=";
+
+        try {
+            URL url = new URL(apiURL);
+            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+            conn.setUseCaches(false);
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+            conn.setRequestProperty("X-OCR-SECRET", secretKey);
+
+            JSONObject json = new JSONObject();
+            json.put("version", "V2");
+            json.put("requestId", UUID.randomUUID().toString());
+            json.put("timestamp", System.currentTimeMillis());
+            
+            JSONObject image = new JSONObject();
+            image.put("format", "jpg");
+            image.put("url", "https://kr.object.ncloudstorage.com/bitbucket/sample.jpg"); // image should be public, otherwise, should use data
+
+//            FileInputStream inputStream = new FileInputStream("YOUR_IMAGE_FILE");
+//            byte[] buffer = new byte[inputStream.available()];
+//            inputStream.read(buffer);
+//            inputStream.close();
+//            image.put("data", buffer);
+            
+            image.put("name", "demo");
+            JSONArray images = new JSONArray();
+            images.put(image);
+            json.put("images", images);
+            
+            // JSON Request 전송
+            String postParams = json.toString();
+            DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
+            wr.writeBytes(postParams);
+            wr.flush();
+            wr.close();
+
+            // JSON Response 처리
+            int responseCode = conn.getResponseCode();
+            BufferedReader br;
+            if (responseCode == 200) {
+                br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            } else {
+                br = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+            }
+            
+            // JSON Response 데이터 가공
+            String jsonString = br.readLine();
+            JSONObject jsonObj = new JSONObject(jsonString);
+            JSONObject jsonImages = jsonObj.getJSONArray("images").getJSONObject(0);
+            JSONArray jsonFields = jsonImages.getJSONArray("fields");
+            for(int i=0; i<jsonFields.length(); i++) {
+            	JSONObject obj = jsonFields.getJSONObject(i);
+            	String name = obj.getString("name");
+            	String inferText = obj.getString("inferText");
+            	System.out.println(name+" : "+inferText);
+            }
+            br.close();
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
