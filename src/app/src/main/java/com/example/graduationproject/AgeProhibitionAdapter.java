6c594194c133fc4ef1ca175e2844279929c1ccff
sth4881 <sth4881@naver.com>
2021-09-13 17:41:21 +0900

    [Update] 애플리케이션 만 나이 계산 방법 추가

--- src/app/src/main/java/com/example/graduationproject/AgeProhibitionAdapter.java
+++ src/app/src/main/java/com/example/graduationproject/AgeProhibitionAdapter.java
@@ -41,26 +41,28 @@ public class AgeProhibitionAdapter {
         return this;
     }
 
-    public ArrayList<ArrayList<String>> getAgeProhibitionData(ArrayList<String> medicineCode) {
+    public ArrayList<ArrayList<String>> getAgeProhibitionData(String[] medicineCode) {
         try {
-            Cursor[] cursor = new Cursor[medicineCode.size()];
-            for(int i=0; i<medicineCode.size(); i++) {
-                String sql = "SELECT * FROM 연령금기 WHERE 제품코드=" + medicineCode.get(i);
-                cursor[i] = db.rawQuery(sql, null);
-            }
-
-            // 처방전에 적혀있는 약 중에서 임부금기에 해당하는 약의 개수만큼 생성
-            ArrayList<ArrayList<String>> arr = new ArrayList<ArrayList<String>>(cursor.length);
-            for(int i=0; i<cursor.length; i++)
+            StringBuilder sql = new StringBuilder();
+            sql.append("SELECT * FROM 연령금기 WHERE 제품코드 = ").append(medicineCode[0]);
+            for(int i=1; i<medicineCode.length; i++)
+                sql.append(" OR 제품코드 = ").append(medicineCode[i]);
+            Cursor cursor = db.rawQuery(sql.toString(), null);
+            
+            // 처방전에 적혀있는 약 중에서 연령금기에 해당하는 약의 개수만큼 생성
+            ArrayList<ArrayList<String>> arr = new ArrayList<ArrayList<String>>();
+            for(int i=0; i<cursor.getCount(); i++)
                 arr.add(new ArrayList<String>());
 
             // 연령금기 테이블의 '제품코드', '제품명', '특정연령', '특정연령단위-1', '특정연령단위-2' 칼럼 데이터를 불러옴
+            cursor.moveToFirst();
             for(int i=0; i<arr.size(); i++) {
-                arr.get(i).add(cursor[i].getString(0)); // 제품코드
-                arr.get(i).add(cursor[i].getString(1)); // 제품명
-                arr.get(i).add(cursor[i].getString(2)); // 특정연령
-                arr.get(i).add(cursor[i].getString(3)); // 특정연령단위-1
-                arr.get(i).add(cursor[i].getString(4)); // 특정연령단위-2
+                arr.get(i).add(cursor.getString(0)); // 제품코드
+                arr.get(i).add(cursor.getString(1)); // 제품명
+                arr.get(i).add(cursor.getString(2)); // 특정연령
+                arr.get(i).add(cursor.getString(3)); // 특정연령단위-1
+                arr.get(i).add(cursor.getString(4)); // 특정연령단위-2
+                cursor.moveToNext();
             }
             return arr;
         } catch (SQLException sqlException) {
--- src/app/src/main/java/com/example/graduationproject/CheckOCRResultActivity.java
+++ src/app/src/main/java/com/example/graduationproject/CheckOCRResultActivity.java
@@ -8,20 +8,40 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class CheckOCRResultActivity extends AppCompatActivity {
     private TextView tvInfo;
     private ListView lvInfo;
     private TextView tvAgeProhibition;
-    private TextView tvCombiProhibition;
+//    private TextView tvCombiProhibition;
     private TextView tvPregnantProhibition;
 
     public ArrayList<ArrayList<String>> ageProhibitionData, combiProhibitionData, pregnantProhibitionData;
 
+    // 만 나이 계산 함수
+    public static int getAmericanAge(String rrn) {
+        Calendar cal = Calendar.getInstance();
+        int calYear = cal.get(Calendar.YEAR);
+        int calMonth = cal.get(Calendar.MONTH)+1;
+        int calDay = cal.get(Calendar.DAY_OF_MONTH);
+
+        int birthYear = Integer.parseInt(rrn.substring(0,2));
+        int birthMonth = Integer.parseInt(rrn.substring(3,4));
+        int birthDay = Integer.parseInt(rrn.substring(4,6));
+        int sex = Integer.parseInt(rrn.substring(7,8));
+
+        int age;
+        if(sex<3) age = calYear - (1900 + birthYear) + 1; // 2000년대 이전 출생
+        else age = calYear - (2000 + birthYear) + 1; // 2000년대 이후 출생
+        if(birthMonth*100 + birthDay < calMonth*100 + calDay) age--; // 생일이 아직 지나지 않은 경우
+        return age;
+    }
+
     private void loadDatabaseData(String[] medicineCode) {
-//        AgeProhibitionAdapter ageProhibitionAdapter = new AgeProhibitionAdapter(getApplicationContext());
-//        ageProhibitionAdapter.create();
-//        ageProhibitionAdapter.open();
+        AgeProhibitionAdapter ageProhibitionAdapter = new AgeProhibitionAdapter(getApplicationContext());
+        ageProhibitionAdapter.create();
+        ageProhibitionAdapter.open();
 
 //        CombiProhibitionAdapter combiProhibitionAdapter = new CombiProhibitionAdapter(getApplicationContext());
 //        combiProhibitionAdapter.create();
@@ -31,11 +51,11 @@ public class CheckOCRResultActivity extends AppCompatActivity {
         pregnantProhibitionAdapter.create();
         pregnantProhibitionAdapter.open();
 
-//        ageProhibitionData = ageProhibitionAdapter.getAgeProhibitionData(medicineCode);
+        ageProhibitionData = ageProhibitionAdapter.getAgeProhibitionData(medicineCode);
 //        combiProhibitionData = combiProhibitionAdapter.getCombiProhibitionData(medicineCode);
         pregnantProhibitionData = pregnantProhibitionAdapter.getPregnantProhibitionData(medicineCode);
 
-//        ageProhibitionAdapter.close();
+        ageProhibitionAdapter.close();
 //        combiProhibitionAdapter.close();
         pregnantProhibitionAdapter.close();
     }
@@ -48,37 +68,38 @@ public class CheckOCRResultActivity extends AppCompatActivity {
         tvInfo = (TextView)findViewById(R.id.tvInfo);
         lvInfo = (ListView)findViewById(R.id.lvInfo);
         tvAgeProhibition = (TextView)findViewById(R.id.tvAgeProhibition);
-        tvCombiProhibition = (TextView)findViewById(R.id.tvCombiProhibition);
+//        tvCombiProhibition = (TextView)findViewById(R.id.tvCombiProhibition);
         tvPregnantProhibition = (TextView)findViewById(R.id.tvPregnantProhibition);
 
         String user_name = getIntent().getStringExtra("user_name");
-        String user_birthday = getIntent().getStringExtra("user_birthday");
+        String user_rrn = getIntent().getStringExtra("user_rrn");
         String hospital_name = getIntent().getStringExtra("hospital_name");
         String hospital_call = getIntent().getStringExtra("hospital_call");
         String doctor_name = getIntent().getStringExtra("doctor_name");
         String pharmacy_name = getIntent().getStringExtra("pharmacy_name");
         String pharmacist_name = getIntent().getStringExtra("pharmacist_name");
         String compound_date = getIntent().getStringExtra("compound_date");
-        String medicine_name1 = getIntent().getStringExtra("medicine_name1");
-        String medicine_name2 = getIntent().getStringExtra("medicine_name2");
-        String medicine_name3 = getIntent().getStringExtra("medicine_name3");
-        String medicine_name4 = getIntent().getStringExtra("medicine_name4");
-        String medicine_name5 = getIntent().getStringExtra("medicine_name5");
-        String medicine_name6 = getIntent().getStringExtra("medicine_name6");
-        String medicine_name7 = getIntent().getStringExtra("medicine_name7");
-        String medicine_name8 = getIntent().getStringExtra("medicine_name8");
-        String medicine_name9 = getIntent().getStringExtra("medicine_name9");
-        String medicine_name10 = getIntent().getStringExtra("medicine_name10");
-        String medicine_name11 = getIntent().getStringExtra("medicine_name11");
-        String medicine_name12 = getIntent().getStringExtra("medicine_name12");
-        String medicine_name13 = getIntent().getStringExtra("medicine_name13");
+        String medicine1 = getIntent().getStringExtra("medicine1");
+        String medicine2 = getIntent().getStringExtra("medicine2");
+        String medicine3 = getIntent().getStringExtra("medicine3");
+        String medicine4 = getIntent().getStringExtra("medicine4");
+        String medicine5 = getIntent().getStringExtra("medicine5");
+        String medicine6 = getIntent().getStringExtra("medicine6");
+        String medicine7 = getIntent().getStringExtra("medicine7");
+        String medicine8 = getIntent().getStringExtra("medicine8");
+        String medicine9 = getIntent().getStringExtra("medicine9");
+        String medicine10 = getIntent().getStringExtra("medicine10");
+        String medicine11 = getIntent().getStringExtra("medicine11");
+        String medicine12 = getIntent().getStringExtra("medicine12");
+        String medicine13 = getIntent().getStringExtra("medicine13");
 
-        // user_birthday를 이용한 user_age 처리
+        // 현재 년도와 user_rrn 정보를 이용한 user_age 처리
+        String user_age = Integer.toString(getAmericanAge(user_rrn));
 
         // 처방전 기본 정보 텍스트뷰로 표시
         StringBuilder sb = new StringBuilder();
         if(user_name != null)
-            sb.append("환자 성명 : ").append(user_name).append("\n");
+            sb.append("환자 성명(나이) : ").append(user_name).append("(만 ").append(user_age).append("세)").append("\n");
         if(hospital_name != null)
             sb.append("의료기관 명칭 : ").append(hospital_name).append("\n");
         if(hospital_call != null)
@@ -94,47 +115,62 @@ public class CheckOCRResultActivity extends AppCompatActivity {
         tvInfo.setText(sb.toString());
         
         // 약 정보 리스트뷰로 표시
-        ArrayList<String> medicineName = new ArrayList<String>();
-        if(medicine_name1 != null) medicineName.add(medicine_name1);
-        if(medicine_name2 != null) medicineName.add(medicine_name2);
-        if(medicine_name3 != null) medicineName.add(medicine_name3);
-        if(medicine_name4 != null) medicineName.add(medicine_name4);
-        if(medicine_name5 != null) medicineName.add(medicine_name5);
-        if(medicine_name6 != null) medicineName.add(medicine_name6);
-        if(medicine_name7 != null) medicineName.add(medicine_name7);
-        if(medicine_name8 != null) medicineName.add(medicine_name8);
-        if(medicine_name9 != null) medicineName.add(medicine_name9);
-        if(medicine_name10 != null) medicineName.add(medicine_name10);
-        if(medicine_name11 != null) medicineName.add(medicine_name11);
-        if(medicine_name12 != null) medicineName.add(medicine_name12);
-        if(medicine_name13 != null) medicineName.add(medicine_name13);
+        ArrayList<String> medicine = new ArrayList<String>();
+        if(medicine1 != null) medicine.add(medicine1);
+        if(medicine2 != null) medicine.add(medicine2);
+        if(medicine3 != null) medicine.add(medicine3);
+        if(medicine4 != null) medicine.add(medicine4);
+        if(medicine5 != null) medicine.add(medicine5);
+        if(medicine6 != null) medicine.add(medicine6);
+        if(medicine7 != null) medicine.add(medicine7);
+        if(medicine8 != null) medicine.add(medicine8);
+        if(medicine9 != null) medicine.add(medicine9);
+        if(medicine10 != null) medicine.add(medicine10);
+        if(medicine11 != null) medicine.add(medicine11);
+        if(medicine12 != null) medicine.add(medicine12);
+        if(medicine13 != null) medicine.add(medicine13);
 
         // 약 정보 데이터베이스 조회에 필요한 코드 목록 생성
-        String[] medicineCode = new String[medicineName.size()];
-        for(int i=0; i<medicineName.size(); i++) {
-            String[] temp = medicineName.get(i).split(" ", 2);
+        String[] medicineCode = new String[medicine.size()];
+        for(int i=0; i<medicine.size(); i++) {
+            String[] temp = medicine.get(i).split(" ");
             medicineCode[i] = temp[0];
         }
         
         // 처방전 내 인식된 정보를 ListView에 표시
-        ArrayAdapter<String > adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, medicineName);
+        ArrayAdapter<String > adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, medicine);
         lvInfo.setAdapter(adapter);
 
         // 처방전 양식이 아닌 경우 오류가 종료되는 것을 방지
-        if(medicineCode.length>0) loadDatabaseData(medicineCode);
+        if(medicineCode.length>0) {
+            loadDatabaseData(medicineCode);
 
-        // 임부금기 관련
-        StringBuilder sbPregnantProhibition = new StringBuilder();
-        sbPregnantProhibition.append("임부금기").append("\n");
-
-        int pregnantProhibitionDataSize = pregnantProhibitionData.size();
-        if(pregnantProhibitionDataSize == 0) sbPregnantProhibition.append("없음").append("\n");
-        else {
-            for (int i=0; i<pregnantProhibitionDataSize; i++) {
-                String drugName= pregnantProhibitionData.get(i).get(1);
-                sbPregnantProhibition.append(drugName).append("\n");
+            // 연령금기 관련
+            sb.setLength(0); // StringBuilder 초기화
+            sb.append("연령금기").append("\n");
+            if(ageProhibitionData.size() == 0) sb.append("해당사항 없음").append("\n");
+            else {
+                for(int i=0; i<ageProhibitionData.size(); i++) {
+                    String medicineName = ageProhibitionData.get(i).get(1).split("_")[0]; // 약 이름
+                    String age = ageProhibitionData.get(i).get(2); // 연령
+                    String unit1 = ageProhibitionData.get(i).get(3); // 세, 개월
+                    String unit2 = ageProhibitionData.get(i).get(4); // 이상, 이하, 미만
+                    sb.append(medicineName).append(" (").append(age).append(unit1).append(" ").append(unit2).append(" 복용금지)\n");
+                }
             }
+            tvAgeProhibition.setText(sb.toString());
+
+            // 임부금기 관련
+            sb.setLength(0); // StringBuilder 초기화
+            sb.append("임부금기").append("\n");
+            if(pregnantProhibitionData.size() == 0) sb.append("해당사항 없음").append("\n");
+            else {
+                for (int i=0; i<pregnantProhibitionData.size(); i++) {
+                    String medicineName= pregnantProhibitionData.get(i).get(1).split("_")[0]; // 약 이름
+                    sb.append(medicineName).append("\n");
+                }
+            }
+            tvPregnantProhibition.setText(sb.toString());
         }
-        tvPregnantProhibition.setText(sbPregnantProhibition.toString());
     }
 }
