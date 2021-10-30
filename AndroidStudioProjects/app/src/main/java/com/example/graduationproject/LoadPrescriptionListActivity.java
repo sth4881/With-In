4607a1a5e3a4e112c6ca82fc7d35ce93c394bace
Jinho Song <sth4881@naver.com>
2021-10-30 21:30:09 +0900

    [Update] 🔥 PrescriptionManagementAdapter 처방전 데이터 조회 기능 구현중

--- AndroidStudioProjects/app/src/main/java/com/example/graduationproject/LoadPrescriptionListActivity.java
+++ AndroidStudioProjects/app/src/main/java/com/example/graduationproject/LoadPrescriptionListActivity.java
@@ -3,25 +3,58 @@ package com.example.graduationproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
 public class LoadPrescriptionListActivity extends AppCompatActivity {
-    private ListView lvPrescription;
+    private TextView tvPrescription;
+    private ListView lvPrescriptionList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_load_prescription_list);
 
-        lvPrescription = (ListView)findViewById(R.id.lvPrescription);
+        tvPrescription = (TextView)findViewById(R.id.tvPrescription);
+        lvPrescriptionList = (ListView)findViewById(R.id.lvPrescriptionList);
 
         ArrayList<String> prescriptionList = getIntent().getStringArrayListExtra("prescriptionList");
 
-        // 처방전 내 인식된 정보를 ListView에 표시
+        // 처방전 목록을 ListView에 표시
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, prescriptionList);
-        lvPrescription.setAdapter(adapter);
+        lvPrescriptionList.setAdapter(adapter);
+
+        // 처방전 목록에서 각각의 아이템에 대한 클릭 이벤트
+        lvPrescriptionList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                PrescriptionManagementAdapter prescriptionManagementAdapter = new PrescriptionManagementAdapter(getApplicationContext());
+                prescriptionManagementAdapter.create();
+                prescriptionManagementAdapter.open();
+
+                // 클릭한 아이템의 처방전 제목을 통해서 처방전 내용을 불러옴
+                String prescription_title = (String)parent.getItemAtPosition(position);
+                ArrayList<String> prescriptionData = prescriptionManagementAdapter.getPrescriptionData(prescription_title);
+
+                StringBuilder sb = new StringBuilder();
+                sb.append(prescriptionData.get(0)).append("\n"); // 처방전 제목
+                sb.append("방문 날짜 : ").append(prescriptionData.get(1)).append("\n"); // 방문날짜
+                sb.append("환자 성명(나이) : ").append(prescriptionData.get(2)); // 환자성명
+                sb.append("(만 ").append(prescriptionData.get(3)).append("세)\n"); // 환자나이
+                sb.append("의료기관 명칭 : ").append(prescriptionData.get(4)).append("\n"); // 의료기관명칭
+                sb.append("의료기관 전화번호 : ").append(prescriptionData.get(5)).append("\n"); // 의료기관전화번호
+                sb.append("처방 의료인의 성명 : ").append(prescriptionData.get(6)).append("\n"); // 처방의료인의성명
+                tvPrescription.setText(sb.toString());
+                prescriptionManagementAdapter.close();
+            }
+        });
     }
 }
--- AndroidStudioProjects/app/src/main/java/com/example/graduationproject/PrescriptionManagementAdapter.java
+++ AndroidStudioProjects/app/src/main/java/com/example/graduationproject/PrescriptionManagementAdapter.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
 import android.widget.Toast;
 
 import java.io.IOException;
@@ -42,47 +43,80 @@ public class PrescriptionManagementAdapter {
 
     // 처방전 목록 불러오는 메소드
     public ArrayList<String> getPrescriptionListData() {
-        String sql = "SELECT 처방전이름 FROM 처방전관리";
-        ArrayList<String> arr = new ArrayList<String>();
+        try {
+            String sql = "SELECT 처방전제목 FROM 처방전관리";
+            ArrayList<String> arr = new ArrayList<String>();
 
-        Cursor cursor = db.rawQuery(sql, null);
-        cursor.moveToFirst();
-        for(int i=0; i<cursor.getCount(); i++) {
-            arr.add(cursor.getString(0)); // 처방전제목
-            cursor.moveToNext();
+            Cursor cursor = db.rawQuery(sql, null);
+            cursor.moveToFirst();
+            for(int i=0; i<cursor.getCount(); i++) {
+                arr.add(cursor.getString(0)); // 처방전제목
+                cursor.moveToNext();
+            }
+            return arr;
+        } catch (SQLException sqlException) {
+            throw sqlException;
         }
-        return arr;
     }
 
-    public boolean insertPrescriptionData(String[] prescriptionData) {
-        // 방법 1 (성공)
-//        StringBuilder sb = new StringBuilder();
-//        sb.append("INSERT INTO 처방전관리 VALUES(");
-//        sb.append((char[]) null).append(", '").append(prescriptionData[0]).append("', '");
-//        sb.append(prescriptionData[1]).append("', '").append(prescriptionData[2]).append("', '");
-//        sb.append(prescriptionData[3]).append("', '").append(prescriptionData[4]).append("', '");
-//        sb.append(prescriptionData[5]).append("', '").append(prescriptionData[6]).append("');");
-//        db.execSQL(sb.toString());
+    // 처방전 목록 내의 각각의 데이터를 불러오는 메소드
+    public ArrayList<String> getPrescriptionData(String prescription_title) {
+        try {
+            // 처방전제목이 중복되지 않도록 설계할 것이므로 하나의 튜플 데이터만 불러옴
+            String sql = "SELECT * FROM 처방전관리 WHERE 처방전제목='"+prescription_title+"'";
 
-        // 방법 2 (성공)
-        ContentValues values = new ContentValues();
-        values.put("처방전번호", (byte[]) null);
-        values.put("처방전이름", prescriptionData[0]);
-        values.put("방문날짜", prescriptionData[1]);
-        values.put("환자성명", prescriptionData[2]);
-        values.put("환자나이", prescriptionData[3]);
-        values.put("의료기관명칭", prescriptionData[4]);
-        values.put("의료기관전화번호", prescriptionData[5]);
-        values.put("처방의료인의성명", prescriptionData[6]);
-
-        // 결과 성공/실패시 true/false 반환
-        long result = db.insert("처방전관리", null, values);
-        if(result==-1) {
-            Toast.makeText(mContext.getApplicationContext(), "처방전 생성 실패", Toast.LENGTH_SHORT).show();
-            return false;
+            ArrayList<String> arr = new ArrayList<String>();
+            
+            // 처방전관리 테이블의 '처방전제목', '방문날짜', '환자성명', '환자나이', '의료기관명칭', '의료기관전화번호', '처방의료인의성명' 데이터를 불러옴
+            // 2021.10.30 처방전관리 테이블에 저장된 처방전 데이터를 불러오는 과정에서 오류 발생
+            Cursor cursor = db.rawQuery(sql, null);
+            arr.add(prescription_title); // 처방전제목
+            arr.add(cursor.getString(2)); // 방문날짜
+            arr.add(cursor.getString(3)); // 환자성명
+            arr.add(cursor.getString(4)); // 환자나이
+            arr.add(cursor.getString(5)); // 의료기관명칭
+            arr.add(cursor.getString(6)); // 의료기관전화번호
+            arr.add(cursor.getString(7)); // 처방의료인의성명
+            return arr;
+        } catch (SQLException sqlException) {
+            throw sqlException;
         }
-        Toast.makeText(mContext.getApplicationContext(), "처방전 생성 성공", Toast.LENGTH_SHORT).show();
-        return true;
+    }
+
+    // OCR 적용 결과로 생성된 데이터들을 처방전관리 테이블에 삽입
+    public boolean insertPrescriptionData(String[] prescriptionData) {
+        try {
+            // 방법 1 (성공)
+            //        StringBuilder sb = new StringBuilder();
+            //        sb.append("INSERT INTO 처방전관리 VALUES(");
+            //        sb.append((char[]) null).append(", '").append(prescriptionData[0]).append("', '");
+            //        sb.append(prescriptionData[1]).append("', '").append(prescriptionData[2]).append("', '");
+            //        sb.append(prescriptionData[3]).append("', '").append(prescriptionData[4]).append("', '");
+            //        sb.append(prescriptionData[5]).append("', '").append(prescriptionData[6]).append("');");
+            //        db.execSQL(sb.toString());
+
+            // 방법 2 (성공)
+            ContentValues values = new ContentValues();
+            values.put("처방전번호", (byte[]) null);
+            values.put("처방전제목", prescriptionData[0]);
+            values.put("방문날짜", prescriptionData[1]);
+            values.put("환자성명", prescriptionData[2]);
+            values.put("환자나이", prescriptionData[3]);
+            values.put("의료기관명칭", prescriptionData[4]);
+            values.put("의료기관전화번호", prescriptionData[5]);
+            values.put("처방의료인의성명", prescriptionData[6]);
+
+            // 결과 성공/실패시 true/false 반환
+            long result = db.insert("처방전관리", null, values);
+            if (result == -1) {
+                Toast.makeText(mContext.getApplicationContext(), "처방전 생성 실패", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+            Toast.makeText(mContext.getApplicationContext(), "처방전 생성 성공", Toast.LENGTH_SHORT).show();
+            return true;
+        } catch (SQLException sqlException) {
+            throw sqlException;
+        }
     }
 
     public void close() {
